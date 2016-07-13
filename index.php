4ab8a772d78748ba5e7f78a04bff48b4aa1b7a0f
Tihh Gonçalves <tihh.gon@gmail.com>
2016-07-13 14:04:21 -0300

    Front e Admin Funcionando

--- index.php
+++ index.php
@@ -3,33 +3,10 @@
  * GirafaCMS
  **/
 
-//config_default
-require_once('./bower_components/girafaCMS/config_default.php');
-
-//config
-require_once('./config.php');
-
-$configs = array_merge($config_d, $config);
-
-//joga config pra GLOBALS
-foreach($configs as $k=>$c){
-
-
-  //trata valores virtuais..
-  if(!is_array($c)) {
-
-    $re = "/\\{(.+)\\}/mi";
-    preg_match_all($re, $c, $out);
-
-    if (isset($out[1][0])) {
-      $val = $$out[1][0];
-      $c = str_replace($out[0][0], $val, $c);
-    }
-  }
-  $GLOBALS[$k] = $c;
-}
+//declara path root para includes..
+set_include_path('./');
 
 //carrega..
-require_once('./bower_components/girafaCMS/init.php');
+require_once('bower_components/girafaCMS/init.php');
 
 ?>
