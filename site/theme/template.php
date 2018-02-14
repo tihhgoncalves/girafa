086934998a4a9c90eb655905f7e46882d71df2b8
Tihh Gonçalves <tihh.gon@gmail.com>
2018-02-14 18:09:30 -0200

    Ajustes de CSS

--- site/theme/template.php
+++ site/theme/template.php
@@ -56,39 +56,18 @@
 
 <body>
 
-<div id="main">
-
-    <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>Cabeçalho do Site</h1>
-                </div>
-            </div>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Cabeçalho do Site</h1>
         </div>
-    </header>
-    <!-- CONTEUDO - INICIO -->
-    <? include($FRONT_PAGES_PATH . $fileHtml); ?>
-    <!-- CONTEUDO - FIM -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <p><strong>Rodapé do Site</strong></p>
-                </div>
-            </div>
-        </div>
-    </footer>
+      </div>
+    </div>
+  </header>
+  <!-- CONTEUDO - INICIO -->
+  <? include($FRONT_PAGES_PATH . $fileHtml); ?>
+  <!-- CONTEUDO - FIM -->
 
-</div>
 </body>
 </html>
-<!-- girafaAnalytics -->
-<script type="text/javascript">
-    var girafa_analytics_path = '<?= get_config('BOWER_COMPONENTS_URL'); ?>girafaCMS/';
-
-    var script = document.createElement('script');
-    script.src = girafa_analytics_path + 'analytics.js';
-    document.body.appendChild(script);
-
-</script>
